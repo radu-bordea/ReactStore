976e39d8c5cc1e82232c884a54fd51baa6b7e9c8
radu-bordea <radu.bordea.dev@gmail.com>
2024-05-10 11:31:19 +0200

    add styles to register page plus link to login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,24 @@ const Register = () => {
       <Form
         method="POST"
         className="card w-96 p-8 bg-base-100 shaddow-lg flex flex-col gap-y-4"
-      ></Form>
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput type="text" label="username" name="username" />
+        <FormInput type="email" label="email" name="email" />
+        <FormInput type="password" label="password" name="password" />
+        <div className="mt-4">
+          <SubmitBtn text="register" />
+        </div>
+        <p className="text-center">
+          Already a member?
+          <Link
+            to="/login"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            login
+          </Link>
+        </p>
+      </Form>
     </section>
   );
 };
