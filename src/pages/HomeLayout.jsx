23ca22ca8cdcf114193ff54e59e2635bcfda755c
radu-bordea <radu.bordea.dev@gmail.com>
2024-05-12 13:33:31 +0200

    create navbar component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components";
+import { Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <section className="align-element py-20">
         <Outlet />
       </section>
