d30a08bea439d1419a1bc34b7b76f66d32a04f21
radu-bordea <radu.bordea.dev@gmail.com>
2024-05-30 09:39:57 +0200

    Create Filters componnent with select, range components

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,16 @@
-const FormInput = ({label, name, type, defaultValue}) => {
+const FormInput = ({label, name, type, defaultValue, size}) => {
   return (
-    <label className="form-control">
-      <div className="label">
-        <span className="label-text">{label}</span>
-      </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text capitalize">{label}</span>
+      </label>
       <input
         type={type}
         name={name}
         defaultValue={defaultValue}
-        className="input input-bordered"
+        className={`input input-bordered ${size}`}
       />
-    </label>
+    </div>
   );
 };
 
