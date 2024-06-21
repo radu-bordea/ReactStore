cfde2f4a3fc9f62f768f85876701e9fd40d8d2c6
radu-bordea <radu.bordea.dev@gmail.com>
2024-06-21 11:00:29 +0200

    complete CartItems list

--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,9 +1,19 @@
+import { useSelector } from 'react-redux'; // Import useSelector hook from react-redux
+import CartItem from './CartItem'; // Import the CartItem component
 
-
+// CartItemsList component to display a list of cart items
 const CartItemsList = () => {
-  return (
-    <h1 className="text-3xl">CartItemsList</h1>
-  )
-}
+  // Use useSelector to get cartItems from the Redux store's state
+  const cartItems = useSelector((state) => state.cartState.cartItems);
 
-export default CartItemsList
+  return (
+    <>
+      {cartItems.map((item) => {
+        // Map over cartItems and render a CartItem component for each item
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </>
+  );
+};
+
+export default CartItemsList; // Export the CartItemsList component
