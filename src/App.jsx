a2d047de3da72dab7c0a1f371a3263c55e4e5dc0
radu-bordea <radu.bordea.dev@gmail.com>
2024-07-16 14:55:32 +0200

    implement checkout page and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,12 @@ import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
+import { loader as checkoutLoader } from "./pages/Checkout";
 // actions
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
-import {store} from "./store";
-
+import { action as checkoutAction } from "./components/CheckoutForm";
+import { store } from "./store";
 
 // Create a browser router configuration
 const router = createBrowserRouter([
@@ -78,6 +79,8 @@ const router = createBrowserRouter([
         path: "checkout",
         // Render the Checkout component for the "/checkout" path
         element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         // Define a route for "/orders"
